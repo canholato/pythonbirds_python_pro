c225937a3235081d4c1aec6fd3aa0b1f2f7d6a5a
canholato <62913988+canholato@users.noreply.github.com>
2020-04-10 17:02:39 -0300

    criado método cumprimentar

---
+++ OO/pessoa2.py
@@ -0,0 +1,9 @@
+class pessoa:
+    def cumprimentar(self):
+        return 'Olá'
+
+if __name__== ' __main__':
+    P = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
